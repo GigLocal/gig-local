7ae4811d6b067f442ae48db53d3f8a0f8368aa21
Marcus Turewicz <24448509+marcusturewicz@users.noreply.github.com>
2021-07-26 18:14:22 +1000

    Add static files from cdn and fallback (#11)
    * Add static files from cdn and fallback
    
    * Fix sdk version
    
    * Use global tools
    
    * Restore libs from src

--- create-local-db.ps1
+++ create-local-db.ps1
@@ -5,7 +5,7 @@ docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=bc21765a-de25-4720-984c-5831dae823
    -p 1433:1433 --name mssql-server-gigs `
    -d mcr.microsoft.com/mssql/server:2017-CU20-ubuntu-16.04
 
-Start-Sleep -s 20
+Start-Sleep -s 30
 
 docker exec -i mssql-server-gigs /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'bc21765a-de25-4720-984c-5831dae8234c' -Q 'CREATE DATABASE gigs'
 
