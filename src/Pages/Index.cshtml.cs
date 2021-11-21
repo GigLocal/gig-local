ac0f62693c875e3e10b6adec2df11c3703d22760
Marcus Turewicz <24448509+marcusturewicz@users.noreply.github.com>
2021-11-21 17:13:30 +1100

    Initial UI (#27)

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -2,16 +2,57 @@
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    private readonly GigContext _context;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    [BindProperty]
+    public IEnumerable<GigRecord> Gigs { get; set; }
+
+    public IndexModel(GigContext context)
     {
-        _logger = logger;
+        _context = context;
     }
+
+    public record GigRecord
+    (
+        string Date,
+        string Time,
+        Decimal TicketPrice,
+        string TicketWebsite,
+        string ArtistName,
+        string ArtistDescription,
+        string ArtistWebsite,
+        string ArtistImage,
+        string VenueName,
+        string VenueWebsite,
+        string VenueAddress
+    );
 
     public void OnGet()
     {
+        var startDate = DateTime.Now;
+        var endDate = startDate.AddDays(7);
+        var gigsQuery = _context.Gigs
+            .AsNoTracking()
+            .Include(g => g.Artist)
+            .Include(g => g.Venue)
+            .Where(g => g.Date >= startDate
+                        && g.Date <= endDate
+                        && EF.Functions.Like(g.Venue.Address, $"%, Northcote VIC%"))
+            .OrderBy(g => g.Date)
+            .ToArray();
 
+        Gigs = gigsQuery.Select(g => new GigRecord(
+            g.Date.ToDayOfWeekDateMonthName(),
+            g.Date.ToTimeHourMinuteAmPm(),
+            g.TicketPrice,
+            g.TicketWebsite,
+            g.Artist.Name,
+            g.Artist.Description,
+            g.Artist.Website,
+            g.Artist.ImageUrl,
+            g.Venue.Name,
+            g.Venue.Website,
+            g.Venue.Address));
     }
 
     public async Task<IActionResult> OnPostLogoutAsync()
