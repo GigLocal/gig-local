b776548db88a1ceda92a01c2a74b1baf3fca675c
Marcus Turewicz <24448509+marcusturewicz@users.noreply.github.com>
2022-07-24 10:43:58 +1000

    Return sitemap only in Production (#156)

--- src/Pages/Sitemap.cshtml.cs
+++ src/Pages/Sitemap.cshtml.cs
@@ -3,14 +3,21 @@ namespace GigLocal.Pages;
 public class SitemapModel : PageModel
 {
     private readonly ISiteMapService _siteMapService;
+    private readonly IWebHostEnvironment _env;
 
-    public SitemapModel(ISiteMapService siteMapService)
+    public SitemapModel(ISiteMapService siteMapService, IWebHostEnvironment env)
     {
         _siteMapService = siteMapService;
+        _env = env;
     }
 
     public async Task<IActionResult> OnGet()
     {
+        if (!_env.IsProduction())
+        {
+            return NotFound();
+        }
+
 		return new ContentResult
 		{
 			ContentType = MediaTypeNames.Text.Xml,
